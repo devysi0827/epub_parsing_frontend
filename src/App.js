05a71dfe069c09f67647419473c0f33183d97f1a
devysi0827 <devysi0827@gmail.com>
2022-03-17 23:33:19 +0900

    django til

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
-import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
+// import { observable } from 'mobx';
+// import { observer } from 'mobx-react';
 
 import MainApp from './Component/MainApp'
 import Sidebar from './Component/Sidebar'
@@ -19,45 +18,6 @@ import Sidebar from './Component/Sidebar'
 
 
 function App() {
-
-  /// 0. 변수모음
-  const [spine,setSpine] = useState("spine is null!!")
-  const [content,setContent] = useState("content is null!!!")
-  const [imageUrl,setImageUrl] = useState("")
-
-  // 1. 백엔드로 파일 보내기
-  const sendToBack = function() {
-
-    // 세팅
-    // console.log("start send")
-    var fileData = new FormData();
-    var uploadFile =  document.getElementById("file");
-    fileData.append("file",uploadFile.files[0], 'myfile')
-    
-    // 전송
-    axios.post('/loader/getfile/', fileData, {
-      headers: {
-        'Content-Type' : 'multipart/form-data'
-      }
-    }).then ((response) => {
-      console.log("send success")
-      console.log(response)
-      // console.log(response.data.image)
-      // console.log(response.data.content)
-      setSpine(response.data.spine)
-      setContent(response.data.xhtml)
-      setImageUrl(response.data.image)
-    }).catch ((err) => {
-      console.log("send fail")
-      // console.log(err)
-    })
-  }
-
-  // const test = function(){
-  //   var testVar =  document.getElementById("image");
-  //   console.log(testVar)
-  //   console.log(imageUrl)
-  // }
   
   return (
     <div className="App">
@@ -65,28 +25,6 @@ function App() {
         <Sidebar/>
       </LeftDiv>
       <MainApp/>
-
-
-      {/* <input style={{backgroundColor:'red'}} type="file" id="file" name="file" onChange={sendToBack} multiple />
-      <hr/>
-      <p style= {{color:"green"}}>spine</p>
-      <div id="spine">{spine}</div>
-      <hr/>
-      <p style= {{color:"green"}}>content</p>
-      <div id="content"> {content} </div>
-      <hr/>
-      <p style= {{color:"green"}}>image</p>
-      <div id="image"> 
-          {imageUrl 
-          ? <img 
-            id ="img"
-            style={{width:500, height:500}} 
-            src={`data:image/jpg;base64,${imageUrl}`} 
-            alt="image is null"
-          /> 
-          : ''}
-      </div>
-      <button onClick={test}>test</button> */}
     </div>
     
   );
@@ -97,7 +35,7 @@ export default App;
 
 const LeftDiv = styled.div`
   left:0;
-  width: 100px;
+  width: 500px;
   border: 1px solid black;
   min-height: 100vh;
 `;
--- src/Component/Sidebar.js
+++ src/Component/Sidebar.js
@@ -1,8 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+// import styled from 'styled-components';
+// import { observable } from 'mobx';
+// import { observer } from 'mobx-react';
 
 function Sidebar() {
+  
+  /// 0. 변수모음
+  const [title,setTitle] = useState("title is null!!")
+  const [spine,setSpine] = useState(['null'])
+  const [manifest,setManifest] = useState(['null'])
+  const [content,setContent] = useState("content is null!!!")
+  const [imageUrl,setImageUrl] = useState("")
+
+  // 1. 백엔드로 파일 보내기
+  const sendToBack = function() {
+
+    // 세팅
+    var fileData = new FormData();
+    var uploadFile =  document.getElementById("file");
+    fileData.append("file",uploadFile.files[0], 'myfile')
+    
+    // 전송
+    axios.post('/loader/opf/', fileData, {
+      headers: {
+        'Content-Type' : 'multipart/form-data'
+      }
+    }).then ((response) => {
+      console.log(response)
+      setTitle(response.data.title)
+      setSpine(response.data.spine)
+      setManifest(response.data.manifest)
+      // setContent(response.data.xhtml)
+      // setImageUrl(response.data.image)
+    }).catch ((err) => {
+      console.log(err)
+    })
+  }
+
+    //3. 버튼 누르면 해당 html backend로 연락보내기
+    const WantPageInfo = function(page) {
+      console.log(page)
+      
+    }
+
+    //4. 버튼 누르면 해당 file backend로 연락보내기
+    const WantFileInfo = function(file) {
+      var fileData = new FormData();
+      var uploadFile =  document.getElementById("file");
+      fileData.append("file",uploadFile.files[0], 'myfile')
+      fileData.append("name",file)
+      
+       // 전송
+      axios.post('/loader/finder/', fileData, {
+        headers: {
+          'Content-Type' : 'multipart/form-data'
+        }
+      }).then ((response) => {
+        console.log(response)
+        if (response.data.file === 'html') {
+
+          setContent(response.data.data)
+        }
+
+        // setTitle(response.data.title)
+        // setSpine(response.data.spine)
+        // setManifest(response.data.manifest)
+        else {
+
+          setImageUrl(response.data.data)
+        }
+      }).catch ((err) => {
+        console.log(err)
+      })
+    }
+
+
   return (
-    <div>Sidebar</div>
+    <div>
+      <input style={{backgroundColor:'red'}} type="file" id="file" name="file" onChange={sendToBack} multiple />
+      <hr/>
+      <p style= {{color:"green"}}>title</p>
+      <div id="spine">{title}</div>
+      <hr/>
+      <p style= {{color:"green"}}>spine</p>
+      <div id="spine">
+        {spine.map((page,index) =>
+            <button key= {index} onClick={(e) => WantPageInfo(page)}> 
+              {page}
+            </button>
+        )}
+      </div>
+      <hr/>
+      <p style= {{color:"green"}}>manifest</p>
+      <div id="spine">
+        {manifest.map((file,index) =>
+            <button key= {index} onClick={(e) => WantFileInfo(file)}> 
+              {file}
+            </button>
+        )}
+      </div>
+      <hr/>
+      <p style= {{color:"green"}}>content</p>
+      <div id="content"> {content} </div>
+      <hr/>
+      <p style= {{color:"green"}}>image</p>
+      <div id="image"> 
+          {imageUrl 
+          ? <img 
+            id ="img"
+            style={{width:500, height:500}} 
+            src={`data:image/jpg;base64,${imageUrl}`} 
+            alt=" is null"
+          /> 
+          : ''}
+      </div>
+    </div>
   )
 }
 
